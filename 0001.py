5f2b89d0760538adaac1084e0346250d22f8e128
Your Name <gim.kobayashi@gmail.com>
2018-07-26 21:58:04 +0900

    update

---
+++ 0001.py
@@ -0,0 +1,4 @@
+
+xs = [int(input()) for i in range(10)]
+xs = list(reversed(sorted(xs)))[:3]
+[print(x) for x in xs ] 
---
+++ 0002.py
@@ -0,0 +1,8 @@
+
+while True:
+  try:
+    print(len(str(sum(list(map(int,input().split()))))))
+  except EOFError:
+    break
+    
+
